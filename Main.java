e3f0808577073f99c97fbcb5099b7da9e07c8257
serlimardiyanti20 <103170287+serlimardiyanti20@users.noreply.github.com>
2022-04-22 23:44:18 +0700

    Tugas Looping 2

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        var a = 10;
+
+        for(; a <= 20 ;){
+            System.out.println("perulangan" + a);
+
+            a++;
+        }
+
+             int b = 10;
+            while( b <= 20 ){
+            System.out.println("perulangan" + b);
+
+            b++;
+
+        }
+
+            int c = 10;
+        do{
+            System.out.println("perulangan ke-" + c);
+            c++;
+        } while (c <= 20);
+
+    }
+}
