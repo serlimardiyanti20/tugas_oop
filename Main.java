cd4b48911770444d4a146c9b3d8525feb16a7493
serlimardiyanti20 <103170287+serlimardiyanti20@users.noreply.github.com>
2022-05-09 01:03:11 +0700

    inheritance

--- Main.java
+++ Main.java
@@ -1,29 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Private
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
-        var a = 10;
-
-        for(; a <= 20 ;){
-            System.out.println("perulangan" + a);
-
-            a++;
-        }
-
-             int b = 10;
-            while( b <= 20 ){
-            System.out.println("perulangan" + b);
-
-            b++;
-
-        }
-
-            int c = 10;
-        do{
-            System.out.println("perulangan ke-" + c);
-            c++;
-        } while (c <= 20);
-
+public static void main(String[] args) {
+       
+    kucing h1 = new kucing();
+    anjing h2 = new anjing();
+    
+    h1.ciriciri();
+    h2.ciriciri();
     }
 }
---
+++ hewan_inheritance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Private
+ */
+public class hewan_inheritance {
+    void ciriciri(){
+    System.out.println("memiliki 4 kaki");
+    System.out.println("memiliki ekor");
+    } 
+}
